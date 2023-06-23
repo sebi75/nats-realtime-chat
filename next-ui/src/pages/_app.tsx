ff43568c436f687e90e007e05f8538de2362d331
Sebastian Semeniuc <xsebyyy@gmail.com>
2023-06-23 18:35:53 +0300

    remove authentication with next auth for service

--- next-ui/src/pages/_app.tsx
+++ next-ui/src/pages/_app.tsx
@@ -1,17 +1,8 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import "@/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
