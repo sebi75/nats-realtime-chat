13333d5508d7ac1f28f3d2b459c798fe97d72f0b
Sebastian Semeniuc <xsebyyy@gmail.com>
2023-10-01 19:38:17 +0300

    auth-service - more signup validation

--- auth-service/app/auth/dto/signupRequest.go
+++ auth-service/app/auth/dto/signupRequest.go
@@ -18,5 +18,23 @@ func (sr *SignupRequest) Validate() *errs.AppError {
 	if sr.Email == "" {
 		return errs.NewValidationError("Email is required")
 	}
+	if len(sr.Username) > 0 && len(sr.Username) < 3 {
+		return errs.NewValidationError("Username must be at least 3 characters")
+	}
+	if len(sr.Username) > 15 {
+		return errs.NewValidationError("Username must be at most 15 characters")
+	}
+	if len(sr.Email) > 0 && len(sr.Email) < 5 {
+		return errs.NewValidationError("Email must be at least 5 characters")
+	}
+	if len(sr.Email) > 50 {
+		return errs.NewValidationError("Email must be at most 50 characters")
+	}
+	if len(sr.Password) < 5 {
+		return errs.NewValidationError("Password must be at least 5 characters")
+	}
+	if len(sr.Password) > 35 {
+		return errs.NewValidationError("Password must be at most 35 characters")
+	}
 	return nil
 }
